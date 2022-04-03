e56daa05908c4ecdd3735ac8b781c63528dc4527
Ohad Koren <ohad.koren@intl.att.com>
2022-04-03 19:25:41 +0300

    media query width

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,17 +1,31 @@
 import * as React from 'react';
 import { Button, CardActionArea, CardActions, Card, CardContent, CardMedia, Typography } from '@mui/material';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  card: {
+    ["@media only screen and (min-width: 1363px) and (max-width: 1636px)"]: {
+      width: 250
+    },
+    ["@media only screen and (max-width: 1363px)"]: {
+      width: 200
+    }
+  }
+});
 
 const Post = (props) => {
+  const classes = useStyles();
+
   return (
-    <Card sx={{ width: 300, height: 305, backgroundColor: 'rgb(251 255 248)', position: 'relative' }}>
-      <CardActionArea sx={{ marginBottom: '10em' }}>
+    <Card className={classes.card} sx={{ width: 300, height: 312, backgroundColor: 'rgb(251 255 248)', position: 'relative' }}>
+      <CardActionArea sx={{ marginBottom: '10em', height: '88%', cursor: 'default' }}>
         <CardMedia
           component="img"
-          height="140"
+          height="50%"
           image="/assets/default-post.svg"
           alt="green iguana"
         /> 
-        <CardContent>
+        <CardContent sx={{ height: '50%' }}>
           <Typography gutterBottom variant="h5" component="div" sx={{ textAlign: 'center' }}>
             {props.title}
           </Typography>
@@ -20,7 +34,7 @@ const Post = (props) => {
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions sx={{ width: '100%', backgroundColor: 'rgb(58, 173, 135)', position: 'absolute', bottom: 0 }}>
+      <CardActions sx={{ width: '100%', backgroundColor: 'rgb(58, 173, 135)', position: 'absolute', bottom: 0, height: '12%' }}>
         <Button size="small" color="primary" sx={{ width: '100%', margin: '0 auto', color: 'white' }}>
           See More
         </Button>
