dacc05dcd1d92417bde6873914ced8aaac1e1539
InbarDarshani <82269196+InbarDarshani@users.noreply.github.com>
2022-04-12 17:47:41 +0300

    Drawer and Layout
    Added navigation drawer menu
    Fixed app layout with grid

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
-import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
-import { Container } from "@mui/material";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Grid, Container } from "@mui/material";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from './components/home/Home';
@@ -24,39 +25,42 @@ const appTheme = createTheme({
     },
   },
   typography: {
-    fontFamily: ["'Quicksand'", 'sans-serif'].join(','), 
-    button: { textTransform: "capitalize", fontSize:16, letterSpacing:'0.07em' },
+    fontFamily: ["'Quicksand'", 'sans-serif'].join(','),
+    button: { textTransform: "capitalize", fontSize: 16, letterSpacing: '0.07em' },
   },
 });
 
 const App = () => {
-
   return (
-    <ThemeProvider theme={appTheme}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />} >
-            <Route index element={<Home />} />
-            <Route path="post/:id" element={<Post />} />
-            <Route path="about" element={<About />} />
-            <Route path="*" element={<NoMatch />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />} >
+          <Route index element={<Home />} />
+          <Route path="post/:id" element={<Post />} />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
 const Layout = () => {
-  const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
   return (
-    <div>
-      <Header />
-      <Offset />
-      <Outlet />
-      <Offset />
-      <Footer />
-    </div>
+    <ThemeProvider theme={appTheme}>
+      <Grid
+        component='layout'
+        container
+        direction="column"
+        display='grid'
+        height='100%'
+        gridTemplateRows='auto 1fr auto'
+      >
+        <Grid item component='header'><Header /></Grid>
+        <Grid item component='main'><Outlet /></Grid>
+        <Grid item component='footer'><Footer /></Grid>
+      </Grid>
+    </ThemeProvider>
   );
 };
 
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,39 +1,63 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { useNavigate } from "react-router-dom";
-import { AppBar, Box, Toolbar, IconButton, Typography, Menu, Container, Avatar, Button, Tooltip, MenuItem } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Box, Toolbar, IconButton, Typography, Menu, Container, Button, Tooltip, MenuItem, Drawer, List, ListItem, ListItemText, ListItemIcon } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import EventIcon from '@mui/icons-material/Event';
 import { UserImageThumbnail } from '../common/Images';
-
-const pages = ['Groceries', 'Events'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const Header = () => {
   let navigate = useNavigate();
 
-  const [anchorElNav, setAnchorElNav] = useState(null);
-  const [anchorElUser, setAnchorElUser] = useState(null);
-
-  const handleOpenNavMenu = (event) => setAnchorElNav(event.currentTarget);
-  const handleOpenUserMenu = (event) => setAnchorElUser(event.currentTarget);
-  const handleCloseNavMenu = () => setAnchorElNav(null);
-  const handleCloseUserMenu = () => setAnchorElUser(null);
-
+  //Navigation menu setup
+  const [navDrawerState, setNavDrawerState] = useState(false);
+  const toggleNavDrawer = (open) => (event) => {
+    if (event.type === "keydown" && (event.key === "Tab" || event.key === "Shift"))
+      return;
+    open ? setNavDrawerState(true) : setNavDrawerState(false);
+  };
   const handleClickItemNavMenu = (event) => {
-    setAnchorElNav(null);
     const target = event.currentTarget.innerText.toLowerCase();
     navigate("./" + target, {});
   };
+
+  const pagesAndIcons = {
+    Groceries: <ShoppingBasketIcon />,
+    Events: <EventIcon />
+  }
+  const navMenu =
+    Object.keys(pagesAndIcons).map((page) => (
+      <MenuItem button key={page} onClick={handleClickItemNavMenu}>
+        <ListItemIcon>{pagesAndIcons[page]}</ListItemIcon>
+        <ListItemText primary={page} />
+      </MenuItem>
+    ));
+
+  //User menu setup
+  const [userMenuAnchorEl, setUserMenuAnchorEl] = useState(null);
+  const handleOpenUserMenu = (event) => setUserMenuAnchorEl(event.currentTarget);
+  const handleCloseUserMenu = () => setUserMenuAnchorEl(null);
 
   const handleClickItemUserMenu = (event) => {
-    setAnchorElUser(null);
+    setUserMenuAnchorEl(null);
     const target = event.currentTarget.innerText.toLowerCase();
     navigate("./" + target, {});
   };
 
+  const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+  const userMenu =
+    settings.map((setting) => (
+      <MenuItem key={setting} onClick={handleClickItemUserMenu}>
+        <Typography textAlign="center">{setting}</Typography>
+      </MenuItem>
+    ));
+
+  //User Data Setup
   const [userData, setUser] = useState({});
   const [noUser, setUserState] = useState(() => {
 
-    //TODO: add request to get user's status
+    //TODO: add request to get user's status and data
 
     //if no user is signed in
     //return true;
@@ -55,92 +79,84 @@ const Header = () => {
 
     return false;
   });
-  
+
+  const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
   return (
-    <AppBar position="fixed">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
+    <Box>
+      <AppBar position="fixed">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
 
-          {/* Large Screen Setup */}
-          <Button href='/' sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}>
-            <img src={'assets/logo_label_white.png'} height='30px' width='100px' />
-          </Button>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button key={page} onClick={handleClickItemNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}>
-                {page}
-              </Button>
-            ))}
-          </Box>
+            {/* Large Screen Setup */}
+            <Button href='/' sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}>
+              <img src={'assets/logo_label_white.png'} height='30px' width='100px' />
+            </Button>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {navMenu}
+            </Box>
 
-          {/* Small Screen Setup */}
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon sx={{ color: 'white' }} />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              keepMounted
-              disableScrollLock
-              anchorEl={anchorElNav}
-              anchorOrigin={{ vertical: 'bottom', horizontal: 'left', }}
-              transformOrigin={{ vertical: 'top', horizontal: 'left', }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{ display: { xs: 'block', md: 'none' }, }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleClickItemNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Button href='/' sx={{ justifyContent: 'left', flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <img src={'assets/logo_label_white.png'} height='30px' width='100px' />
-          </Button>
-
-          {/* Profile Icon Setup */}
-          <Box sx={{ flexGrow: 0 }} hidden={Boolean(noUser)}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <UserImageThumbnail src={userData.profileImage}/>
+            {/* Small Screen Setup */}
+            <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={toggleNavDrawer(true)}
+                color="inherit"
+              >
+                <MenuRoundedIcon sx={{ color: 'white' }} />
               </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              keepMounted
-              disableScrollLock
-              anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-              transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleClickItemUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+              <Drawer
+                anchor="left"
+                disableScrollLock
+                open={navDrawerState}
+                onClose={toggleNavDrawer(false)}>
+                <Box
+                  onClick={toggleNavDrawer(false)}
+                  onKeyDown={toggleNavDrawer(false)}
+                >
+                  <List>
+                    {navMenu}
+                  </List>
+                </Box>
+              </Drawer>
+            </Box>
+            <Button href='/' sx={{ justifyContent: 'left', flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <img src={'assets/logo_label_white.png'} height='30px' width='100px' />
+            </Button>
 
-          {/* Profile Icon Setup */}
-          <Box sx={{ flexGrow: 0 }} hidden={Boolean(!noUser)}>
-            <Button sx={{ color: 'white' }}>Sign In \ Create account</Button>
-          </Box>
+            {/* Profile Icon Setup */}
+            <Box sx={{ flexGrow: 0 }} hidden={Boolean(noUser)}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <UserImageThumbnail src={userData.profileImage} />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={userMenuAnchorEl}
+                keepMounted
+                disableScrollLock
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+                open={Boolean(userMenuAnchorEl)}
+                onClose={handleCloseUserMenu}
+              >
+                {userMenu}
+              </Menu>
+            </Box>
 
-        </Toolbar>
-      </Container>
-    </AppBar>
+            {/* Profile Icon Setup */}
+            <Box sx={{ flexGrow: 0 }} hidden={Boolean(!noUser)}>
+              <Button sx={{ color: 'white' }}>Sign In \ Create account</Button>
+            </Box>
+
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Offset />
+    </Box>
   );
 };
 
