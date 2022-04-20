d812a0c7141b7687d0569d97571f1efe2c74fe35
mor <morex48562@gmail.com>
2022-04-20 16:29:49 +0300

    add registration page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -33,6 +33,7 @@ const App = () => {
           <Route path="about" element={<About />} />
           <Route path="login" element={<Login/>} />
           <Route path="*" element={<NoMatch />} />
+          <Route path="register" element={<Register/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
