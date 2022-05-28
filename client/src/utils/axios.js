8b200fb07b240535c8844261f72654f3f99b02eb
AgoatZ <sk8amit@gmail.com>
2022-05-28 17:38:35 +0300

    host

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
 const instance = axios.create({
-  //baseURL: "http://localhost:5000/api/",
-  baseURL: "https://grosharies.herokuapp.com/api",
+  baseURL: "http://localhost:5000/api/",
   timeout: 20000,
   withCredentials: true,
   headers: {
-    "Access-Control-Allow-Origin": "https://grosharies.herokuapp.com/",
+    "Access-Control-Allow-Origin": "http://localhost:3000",
     "Access-Control-Allow-Headers": "*",
     "Access-Control-Allow-Credentials": "true",
     "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
