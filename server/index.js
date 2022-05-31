1a00453b6e041160a10af0a665b1549dbc2e27d0
AgoatZ <sk8amit@gmail.com>
2022-05-31 17:19:22 +0300

    resolve

--- server/index.js
+++ server/index.js
@@ -40,8 +40,8 @@ if (process.env.NODE_ENV == "development") {
   app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 }
 
-//app.use(express.static(path.join(__dirname, '/client/build')));
-app.use(express.static(path.join(__dirname, 'build')));
+//app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 app.use(bodyParser.json({ limit: '16mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
