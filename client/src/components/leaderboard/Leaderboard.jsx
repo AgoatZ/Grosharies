e567dd2fec5815caf335827677c362e611d8c365
Ran Berant <ranberant@gmail.com>
2022-06-11 17:37:32 +0300

    Set infra for leaderboard page

--- client/src/components/leaderboard/Leaderboard.jsx
+++ client/src/components/leaderboard/Leaderboard.jsx
@@ -1,11 +1,31 @@
-import { Container, Typography } from '@mui/material';
+import { Container, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Leaderboard = () => {
+  const [leaderboard, setLeaderboard] = useState({});
+  useEffect(() => {
+    axios.get("/api/leaderboards", () => {
+      setLeaderboard(leaderboard);
+    });
+  }, []);
+
+  if (!leaderboard) {
     return (
-        <Container>
-            <Typography variant="h5">Leaderboard</Typography>
-        </Container>
+      <Container>
+        <table>
+          {leaderboard.map((leader) => {
+            return (
+              <tr>
+                <td>{leader.name} </td>
+                <td>{leader.rank} </td>
+              </tr>
+            );
+          })}
+        </table>
+      </Container>
     );
-}
+  }
+};
 
 export default Leaderboard;
