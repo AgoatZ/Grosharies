12462bfb9d9c72183848f98d55eee6ea0448d103
AgoatZ <sk8amit@gmail.com>
2022-06-13 02:00:39 +0300

    delete comments

--- server/src/common/middlewares/image-upload.js
+++ server/src/common/middlewares/image-upload.js
@@ -11,11 +11,6 @@ const uploadImage = async (req, res, next) => {
           throw Error(error);
         });
 
-        // req.on('data', function (chunk, error) {
-        //   console.log('here');
-        //   chData.push(chunk);
-        // });
-
         req.on('end', async (error) => {
           console.log('no more data');
           data = fs.readFileSync(newFile.path, "base64");
@@ -31,8 +26,6 @@ const uploadImage = async (req, res, next) => {
       
       newFile.on('close', () => {
         console.log('closing file');
-        //const enc = Buffer.from(chData.toString()).toString("base64");
-        //const enc = chData.toString("base64");
         req.body = { image: data };
         next();
       });
