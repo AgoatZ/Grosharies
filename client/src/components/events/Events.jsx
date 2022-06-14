decef47db90484805122b56a6a19cfba8e91570c
InbarDarshani <InbarProjects@gmail.com>
2022-06-14 03:34:18 +0300

    Update Events.jsx

--- client/src/components/events/Events.jsx
+++ client/src/components/events/Events.jsx
@@ -3,7 +3,7 @@ import { Container, Typography } from '@mui/material';
 const Events = () => {
   return (
     <Container>
-      <Typography sx={{ fontWeight: "bold", fontSize: "30px", margin: "3em 11em" }} variant="h5">No Event At The Moment</Typography>
+      <Typography variant="h5" sx={{ fontWeight: "bold", fontSize: "30px" }} >No Event At The Moment</Typography>
     </Container>
   );
 }
